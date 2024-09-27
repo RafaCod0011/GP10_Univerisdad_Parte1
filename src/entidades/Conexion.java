1a84da45905003cf99c5d060a04be4536abacddf
FacuMartinGarcia <estudiodesarrolladorsoftware@gmail.com>
2024-09-27 11:04:38 -0300

    Clase conexion
    * clase conexion finalizada; se incorpora libreria mariaDB

--- src/entidades/Conexion.java
+++ src/entidades/Conexion.java
@@ -1,40 +1,42 @@
 
 package entidades;
 
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 
 public class Conexion {
 
-    private static final String URL="jdbc:mysql://localhost/";
-    private static final String DB="GP10_universidadulp";
-    private static final String USUARIO="root";
-    private static final String PASSWORD="";
-    
+    private static final String URL = "jdbc:mysql://localhost/";
+    private static final String DB = "gp10_universidadulp";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+
     private static Connection connection;
 
     private Conexion() {}
 
-    public static Connection getConexion(){
-    
-      if(connection == null){
-          try {
-              Class.forName("");
-              //connection = DriveManager.
-              
-              
-          } catch (Exception e) {
-          }
-              
-      }    
-    
-    
+    public static Connection getConexion() {
+
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC", 
+                                                          USUARIO, PASSWORD);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la BD " + ex.getMessage());
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers " + ex.getMessage());
+            }
+        }
+
         return connection;
+
     }
-    
-    
-    
-    
-    
-    
 }
+    
+    
+    
+
